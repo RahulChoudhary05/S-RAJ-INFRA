8ce0e3b98430da8e552538f5201d60550da62a84
Singhmayank0217 <coolsinghmayank@gmail.com>
2024-10-23 14:44:04 +0530

    added gallery hhome page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { NavBar } from './components/common/NavBar';
+import ImageSlider from './components/HomeGallery/gallery';
 
 function App() {
   return (
     <div className="w-screen min-h-screen bg-white flex flex-col font-inter">
       <NavBar/>
+      <ImageSlider/>
     </div>
   );
 }
