faa0a69365784453d946c977d7980471ea4b3e26
Singhmayank0217 <coolsinghmayank@gmail.com>
2024-10-23 15:32:24 +0530

    Footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { NavBar } from './components/common/NavBar';
 import ImageSlider from './components/HomeGallery/gallery';
+import Footer from './components/common/Footer';
 
 function App() {
   return (
     <div className="w-screen min-h-screen bg-white flex flex-col font-inter">
       <NavBar/>
       <ImageSlider/>
+      <Footer/>
     </div>
   );
 }
