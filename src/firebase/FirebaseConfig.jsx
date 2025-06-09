333fb26b4335024a89e29d14d120c50e982793ac
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2025-06-09 12:10:54 +0530

    chore: remove Next.js dependency from package.json
    fix: update Firebase storage bucket URL in FirebaseConfig.jsx

--- src/firebase/FirebaseConfig.jsx
+++ src/firebase/FirebaseConfig.jsx
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBoBjh-QXzEXzpaR4kJxYrU4nz8Vw4huIA",
   authDomain: "srajinfraprojectprivatelimited.firebaseapp.com",
   projectId: "srajinfraprojectprivatelimited",
-  storageBucket: "srajinfraprojectprivatelimited.firebasestorage.app",
+  storageBucket: "srajinfraprojectprivatelimited.appspot.com",
   messagingSenderId: "1091272874105",
   appId: "1:1091272874105:web:ffecdaa93c07cf2c405ca8",
   measurementId: "G-RJNFGVSSH6"
